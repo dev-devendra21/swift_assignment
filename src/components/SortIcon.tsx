f02e65cf787e7b10b557dcd9b9363877d84e7a16
devendra kumar <devendrakumaraa@gmail.com>
2025-07-09 10:38:39 +0530

    Fix the issue in sortIcon

--- src/components/SortIcon.tsx
+++ src/components/SortIcon.tsx
@@ -5,26 +5,26 @@ import {
   PiCaretUpThin,
 } from "react-icons/pi";
 
-type SortIconProps = { sortBy: string; order: string };
-const SortIcon: React.FC<SortIconProps> = ({ sortBy, order }) => {
+type SortIconProps = {
+  column: string;
+  sortBy: string;
+  order: string;
+};
+
+const SortIcon: React.FC<SortIconProps> = ({ column, sortBy, order }) => {
   const className = `ml-1 inline-block w-5 h-5`;
-  const renderIcon = () => {
+
+  if (sortBy === column) {
     return order === "asc" ? (
-      <PiCaretDownThin className={className} />
-    ) : order === "desc" ? (
       <PiCaretUpThin className={className} />
+    ) : order === "desc" ? (
+      <PiCaretDownThin className={className} />
     ) : (
       <PiCaretUpDownThin className={className} />
     );
-  };
-  switch (sortBy) {
-    case "name":
-      return renderIcon();
-    case "email":
-      return renderIcon();
-    case "id":
-      return renderIcon();
   }
+
+  return <PiCaretUpDownThin className={className} />;
 };
 
 export default SortIcon;
